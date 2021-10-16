bb8603a85463509572da537b64c547c71915ab72
Asif-Iqbal-Gazi <asif.iqbal.gazi@gmail.com>
2021-10-15 21:18:54 -0500

    Updated packages, removed jade, added hbs

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
 var createError = require('http-errors');
 var express = require('express');
+var session = require('express-session');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const passport = require('passport');
+var loginRouter = require('./routes/login');
+var createAccountRouter = require('./routes/createAccount');
+var submitRouter = require('./routes/submit');
+var loginSubmitRouter = require('./routes/loginSubmit');
+var dashboardRouter = require('./routes/dashboard.js');
 
 var app = express();
 
+
+//passport config
+require('./config/passport')(passport);
+
+var session_config = {
+		secret: 'secret', //a random unique string key used to authenticate a session
+		resave: true, //nables the session to be stored back to the session store, even if the session was never modified during the request
+		saveUninitialized: true, //his allows any uninitialized session to be sent to the store. When a session is created but not modified, it is referred to as uninitialized.
+		cookie: { secure: true } //true is a recommended option. However, it requires an https-enabled website
+		//store  parameter when saving session to database
+};
+
+session_config.cookie.secure = false;
+//IMPORTANT REVIEW IN CLASS - https://expressjs.com/en/resources/middleware/session.html
+
+//Express Sessions
+app.use(session(session_config))
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +40,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', loginRouter);
+app.use('/createAccount', createAccountRouter);
+app.use('/submit', submitRouter);
+app.use('/loginSubmit', loginSubmitRouter);
+app.use('/dashboard', dashboardRouter);
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    let userInfo = req.user;
+    console.log(req.user);
+
+    res.render('dashboard', {userInfo});
+    
 });
 
 module.exports = router;
